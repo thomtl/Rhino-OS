2a2e663675b7cdb696b0eb544a1c8a43202fe963
thomtl <twoertman@gmail.com>
2018-06-30 10:48:43 +0200

    Basic syscall

--- kernel/user/syscall.c
+++ kernel/user/syscall.c
@@ -2,9 +2,46 @@
 #include "../common.h"
 #include "../../drivers/screen.h"
 #include "../../libc/include/stdio.h"
+#include "../multitasking/task.h"
+
+
+static inline void sys_yield(){
+  yield();
+  return;
+}
+
+static inline void sys_exec(void(*main)()){
+  exec(get_running_task(), main);
+  return;
+}
+
 void syscall_handler(registers_t *regs){
   kprint("\nSYSCALL RECEIVED!\n");
-  UNUSED(regs);
+  switch (regs->eax) {
+    case 0:
+      switch(regs->ebx){
+        case 1:
+          sys_yield();
+          break;
+        case 2:
+          sys_exec((void(*)())regs->ecx);
+          break;
+        default:
+          return;
+      }
+      break;
+    case 1:
+      switch(regs->ebx){
+        case 1:
+          putchar(regs->ecx);
+          break;
+        default:
+          return;
+      }
+      break;
+    default:
+      return;
+  }
   return;
 }
 
--- utils/UserTest/main.asm
+++ utils/UserTest/main.asm
@@ -2,9 +2,8 @@
 [section .text]
 
 main:
-    mov eax, M
+    mov eax, 1
+    mov ebx, 1
+    mov ecx, 'Q'
     int 0x80
     ret
-
-M:
-  db 'Hello World from an external program!', 0
