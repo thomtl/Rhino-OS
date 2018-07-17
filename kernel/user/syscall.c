ac64d0bbaada6bf6363673fed8e1bf7e3b800fe6
thomtl <twoertman@gmail.com>
2018-07-17 14:22:22 +0200

    More syscalls
    return value from syscall

--- kernel/user/syscall.c
+++ kernel/user/syscall.c
@@ -4,6 +4,19 @@
 #include "../../libc/include/stdio.h"
 #include "../multitasking/task.h"
 
+
+static inline task_t* sys_fork(){
+  return fork();
+}
+
+static inline int sys_current_pid(){
+  return get_current_pid();
+}
+
+static inline void sys_clear_screen(){
+  clear_screen();
+}
+
 static inline void sys_start_task_atomic(){
   start_task_atomic();
 }
@@ -43,6 +56,15 @@ void syscall_handler(registers_t *regs){
         case 4:
           sys_exec((void(*)())regs->ecx);
           break;
+        case 5:
+          kprint_err("Exit not implemented");
+          break;
+        case 6:
+          regs->eax = (uint32_t)sys_fork();
+          break;
+        case 7:
+          regs->eax = sys_current_pid();
+          break;
         default:
           return;
       }
@@ -51,6 +73,9 @@ void syscall_handler(registers_t *regs){
       switch(regs->ebx){
         case 1:
           sys_putchar(regs->ecx);
+          break;
+        case 2:
+          sys_clear_screen();
           break;
         default:
           return;
--- utils/UserTest/main.c
+++ utils/UserTest/main.c
@@ -1,12 +1,13 @@
 void start_task_atomic();
 void end_task_atomic();
-void putchar(char c);
+int putchar(char c);
 void printf(char *m);
+void clear_screen();
+#include <stdint.h>
 
 
 void main(void){
-  __asm__ ("sti");
-  putchar('x');
+    __asm__ ("sti");
   printf("Hello World");
   while(1);
 }
@@ -17,12 +18,17 @@ void printf(char *m){
   }
 }
 
-void putchar(char c){
-  __asm__ __volatile__ ("pusha; movl $0x1, %%eax; movl $0x1, %%ebx; movl %%edx, %%ecx; int $0x80; popa" : : "d" (c));
+int putchar(char c){
+  int eax;
+  __asm__ __volatile__ ("movl $0x1, %%eax; movl $0x1, %%ebx; movl %%edx, %%ecx; int $0x80" : "=a"(eax): "d" (c));
+  return eax;
+}
+void clear_screen(){
+  __asm__ __volatile__ ("movl $0x1, %eax; movl $0x2, %ebx; int $0x80");
 }
 void start_task_atomic(){
-  __asm__ __volatile__ ("pusha; movl $0x0, %eax; movl $0x2, %ebx; int $0x80; popa");
+  __asm__ __volatile__ ("movl $0x0, %eax; movl $0x2, %ebx; int $0x80");
 }
 void end_task_atomic(){
-  __asm__ __volatile__ ("pusha; movl $0x0, %eax; movl $0x3, %ebx; int $0x80; popa");
+  __asm__ __volatile__ ("movl $0x0, %eax; movl $0x3, %ebx; int $0x80");
 }
