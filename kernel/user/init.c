3982aa2bddee5a683d9d29e6eca6d06ab7aae6b1
Unknown <twoertman@gmail.com>
2018-07-27 14:49:01 +0200

    Started on Cloning for init

--- kernel/user/init.c
+++ kernel/user/init.c
@@ -3,6 +3,7 @@ extern uintptr_t page_directory;
 
 void init(char *prg){
   uint8_t attr = get_color();
+  uintptr_t dir;
   set_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLUE);
 
   start_task_atomic();
@@ -15,15 +16,17 @@ void init(char *prg){
   kprint("Program Loaded\n");
 
   uint32_t m = (header->len / MM_PAGE_S) + 1;
-
+  kprint("Cloning Dir\n");
+  dir = clone_dir(page_directory);
+  kprint("Cloned Dir\n");
   kprint("Mapping Pages\n");
   uint32_t j = (uint32_t)PROGRAM_LOAD_ADDRESS;
   for(uint32_t i = FREE_PROG; i < (FREE_PROG + m); i += MM_PAGE_S) {
-    map_phys_virt(page_directory, i, j);
+    map_phys_virt(dir, i, j);
     j += MM_PAGE_S;
   }
   kprint("Mapped Pages\n");
-
+  switch_dir(dir);
   memcpy(PROGRAM_LOAD_ADDRESS, header->base, header->len);
   kprint("Program in designated zone\n");
 
@@ -31,13 +34,14 @@ void init(char *prg){
   kprint("Loading Space Freed\n");
 
   task_t* root = task_for_pid(0);
-  createTask(PROGRAM_LOAD_ADDRESS, root->regs.eflags, (uint32_t*)root->regs.cr3);
+  createTask(PROGRAM_LOAD_ADDRESS, root->regs.eflags, (uint32_t*)dir);
 
   kill(0);
   kprint("Kernel Task Killed\n");
 
   kprint("Program Loading Done");
   set_raw_color(attr);
+  //__asm__ ("cli; hlt");
   end_task_atomic();
-
+  //__asm__ ("cli; hlt");
 }
