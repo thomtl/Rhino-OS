2bb0699d4a2202b529714e1ac7936752f03b7950
Thomas Woertman <twoertman@gmail.com>
2018-09-25 17:15:55 +0200

    Fix EOI

--- src/kernel/arch/x86/isr.c
+++ src/kernel/arch/x86/isr.c
@@ -133,15 +133,14 @@ void register_interrupt_handler(uint8_t n, isr_t handler){
 }
 
 void irq_handler(registers_t *r){
-  if(r->int_no >= 40) outb(0xA0, 0x20);
-  outb(0x20, 0x20);
-
 
   if(interrupt_handlers[r->int_no] != 0){
     isr_t handler = interrupt_handlers[r->int_no];
     handler(r);
   }
 
+  if(r->int_no >= 40) outb(0xA0, 0x20);
+  outb(0x20, 0x20);
 
 }
 
