e2ffb9e6718fecd2ad8beeb78ac15283a2effbc2
Thomas Woertman <twoertman@gmail.com>
2019-03-17 15:22:57 +0100

    Add time

--- include/rhino/arch/x86/timer.h
+++ include/rhino/arch/x86/timer.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include <rhino/common.h>
+#include <rhino/arch/x86/isr.h>
+#include <rhino/multitasking/scheduler.h>
+#include <rhino/arch/x86/drivers/pit.h>
+#include <rhino/arch/x86/drivers/hpet.h>
+
+#include <rhino/arch/x86/drivers/rtc.h>
+
 void init_timer();
 uint32_t get_tick();
 void sleep(uint32_t nSeconds);
 void msleep(uint32_t nMseconds);
+time_t now();
--- src/kernel/arch/x86/timer.c
+++ src/kernel/arch/x86/timer.c
@@ -1,45 +1,73 @@
 #include <rhino/arch/x86/timer.h>
-#include <rhino/arch/x86/isr.h>
-#include <rhino/arch/x86/io.h>
-#include <rhino/common.h>
-#include <rhino/multitasking/scheduler.h>
-#include <rhino/arch/x86/drivers/pit.h>
-#include <rhino/arch/x86/drivers/hpet.h>
+
 
 volatile uint32_t irq0s = 0;
 volatile uint32_t irq8s = 0;
 volatile uint32_t seconds = 0;
+time_t time = {0};
+
+static void update_time(){
+	time.second++;
+    if(time.second == 60){
+        time.minute++;
+        time.second = 0;
+    }
+
+    if(time.minute == 60){
+        time.hour++;
+        time.minute = 0;
+	}	
+
+    if(time.hour == 24){
+        time.day++;
+        time.hour = 0;
+    }
+
+	if(time.day > 27){ // leap year
+        // Don't even try
+        time = read_rtc_time();
+  }
+}
+
+
 bool hpet;
 static void irq0_callback(registers_t *regs){
-  irq0s++;
-  if(irq0s % 1000 == 0) seconds++;
+	irq0s++;
+  	if(irq0s % 1000 == 0) {
+		seconds++;
+
+		update_time();	
+  	}
   
-  schedule(regs);
+  	schedule(regs);
 }
 
 static void irq8_callback(registers_t *regs){
-  irq8s++;
-  UNUSED(regs);
+  	irq8s++;
+  	UNUSED(regs);
 }
 
 void init_timer(){
-  register_interrupt_handler(IRQ0, irq0_callback);
-  register_interrupt_handler(IRQ8, irq8_callback);
+  	register_interrupt_handler(IRQ0, irq0_callback);
+  	register_interrupt_handler(IRQ8, irq8_callback);
 
-  debug_log("[TIMER]: Initializing Timer\n");
-  if(detect_hpet()){
-    hpet = false;//init_hpet(100000);
-    if(hpet){
-     debug_log("[TIMER]: Using HPET as main Timer\n");
-    } else {
-      goto pit;
-    }
-  } else {
-    pit:
-    init_pit(1000);
-    debug_log("[TIMER]: Using PIT as main Timer\n");
-  }
-  debug_log("[TIMER]: Initialized Timer\n");
+  	debug_log("[TIMER]: Initializing Timer\n");
+  	if(detect_hpet()){
+		hpet = false;//init_hpet(100000);
+		if(hpet){
+	 		debug_log("[TIMER]: Using HPET as main Timer\n");
+		} else {
+	  		goto pit;
+		}
+  	} else {
+		pit:
+		init_pit(1000);
+		debug_log("[TIMER]: Using PIT as main Timer\n");
+  	}
+
+	time = read_rtc_time();
+	
+  	debug_log("[TIMER]: Initialized Timer\n");
 }
 
 uint32_t get_tick(){
@@ -60,4 +88,8 @@ void msleep(uint32_t nMseconds){
   STI();
   while(irq0s < eticks) asm("hlt");
   load_eflags(eflags);
+}
+
+time_t now(){
+	return time;
 }
